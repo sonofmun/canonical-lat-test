a7665ecaf9e3c56cbb9dec1e58564206cac2cbec
sonofmun <mmunson@gcdh.de>
2017-05-15 15:03:29 +0200

    Removed teiHeader and notes from the plain text

--- plain_text.py
+++ plain_text.py
@@ -7,7 +7,7 @@ Repository = CtsCapitainsLocalResolver(["./"])
 for text in Repository.texts:
     try:
         interactive_text = Repository.getTextualNode(text.id)
-        plaintext = interactive_text.export(Mimetypes.PLAINTEXT) 
+        plaintext = interactive_text.export(Mimetypes.PLAINTEXT, exclude=["tei:note", "tei:teiHeader"]) 
         with open('text/{}.txt'.format(text.id.split(':')[-1]), mode='w') as f:
             f.write(plaintext)
     except Exception as E:
